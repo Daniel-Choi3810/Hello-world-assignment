9d023bba97654d146bc989a9430459d0ad26d635
Daniel Choi <63138113+Daniel-Choi3810@users.noreply.github.com>
2024-02-01 23:05:32 -0500

    Completes the assignment

--- app/src/main/java/edu/virginia/cs/helloworldhw/MainActivity.kt
+++ app/src/main/java/edu/virginia/cs/helloworldhw/MainActivity.kt
@@ -3,13 +3,29 @@ package edu.virginia.cs.helloworldhw
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusProperties
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import edu.virginia.cs.helloworldhw.ui.theme.HelloWorldHWTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +38,76 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(modifier: Modifier = Modifier) {
+    var name by remember { mutableStateOf("Your Name Here!") }
+    var greeting by remember { mutableStateOf("Hello") }
+    var expanded by remember { mutableStateOf(false) }
+    val greetings = listOf("Hello", "Hi There", "Bonjour", "Hola")
+    Column(
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.padding(top = 24.dp)
+    ) {
+        Text(
+            text = "${"$greeting, $name"}!",
+            fontSize = 24.sp,
+            modifier = modifier.padding(bottom = 10.dp)
+        )
+        Column {
+            TextField(
+                label = { Text("Name") },
+                value = name,
+                onValueChange = { name = it },
+                modifier = modifier.padding(bottom = 10.dp)
+            )
+            ExposedDropdownMenuBox(
+                expanded = expanded,
+                onExpandedChange = { expanded = !expanded },
+            ) {
+                TextField(
+                    readOnly = true,
+                    label = { Text("Greeting") },
+                    value = greeting,
+                    onValueChange = {},
+                    trailingIcon = {
+                        ExposedDropdownMenuDefaults.TrailingIcon(
+                            expanded = expanded
+                        )
+                    },
+                    modifier = modifier
+                        .padding(bottom = 10.dp)
+                        .menuAnchor()
+                        .focusProperties { canFocus = false }
+                )
+                ExposedDropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false }) {
+                    greetings.forEach {
+                        DropdownMenuItem(text = { Text(it) }, onClick = {
+                            greeting = it
+                            expanded = false
+                        })
+                    }
+                }
+            }
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     HelloWorldHWTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
